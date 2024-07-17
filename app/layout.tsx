421a9d21ee67e30e40d7e733e6104b4968a14ef8
SqnkaBG - БГ Сянка <100valentin100@gmail.com>
2024-07-17 09:17:08 +0300

    raboti

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,45 +1,47 @@
-// layout.tsx
-
+import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import Head from "next/head";
 import Link from "next/link";
-import Image from "next/image";
 import valentin from "../public/images/valentin-shindarov.png";
 
 const inter = Inter({ subsets: ["latin"] });
 
-interface LayoutProps {
-  children: React.ReactNode;
-}
-
-const Layout: React.FC<LayoutProps> = ({ children }) => {
-  return (
-    <html lang="en">
-      <Head>
-        <meta charSet="UTF-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <title>Valentin Shindarov</title>
-        <meta
-          name="description"
-          content="Personal portfolio showcasing my skills"
-        />
-      </Head>
-      <body className={inter.className}>
-        {/* Image container */}
-        <div className="image-container">
-          <Image
-            src={valentin}
-            alt="Valentin Shindarov"
-            style={{ width: "100%", height: "auto" }}
-          />
-        </div>
-
-        {/* Render the children passed to the layout */}
-        {children}
-      </body>
-    </html>
-  );
+export const metadata = {
+  title: "Valentin Shindarov",
+  description: "Personal portfolio showcasing my skills",
+  openGraph: {
+    title: "Valentin Shindarov",
+    description: "Personal portfolio showcasing my skills",
+    images: [
+      {
+        url: "/images/valentin-shindarov.png",
+        width: 800,
+        height: 600,
+        alt: "Valentin Shindarov",
+      },
+    ],
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Valentin Shindarov",
+    description: "Personal portfolio showcasing my skills",
+    images: [
+      {
+        url: "/images/valentin-shindarov.png",
+        alt: "Valentin Shindarov",
+      },
+    ],
+  },
 };
 
-export default Layout;
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
